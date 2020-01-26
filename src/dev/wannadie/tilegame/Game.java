efbe0c00f3e6a2bbca03cbf5fe9d245f1276240f
Pedro Cisneros Santana <pedro.cisneros.santana@gmail.com>
2020-01-27 00:48:55 +0100

    update Game class

--- src/dev/wannadie/tilegame/Game.java
+++ src/dev/wannadie/tilegame/Game.java
@@ -1,16 +1,31 @@
 package dev.wannadie.tilegame;
 
-public class Game {
+public class Game implements Runnable {
 
   private Display display;
 
   public int width;
   public int height;
 
+  private Thread thread;
+
   public Game(String title, int width, int height) {
 	this.width = width;
 	this.height = height;
 	display = new Display(title, width, height);
+  }
+
+  public void run() {
+
+  }
+
+  public synchronized void start() {
+	thread = new Thread();
+	thread.start();
+  }
+
+  public synchronized void stop() {
+
   }
 
 }
