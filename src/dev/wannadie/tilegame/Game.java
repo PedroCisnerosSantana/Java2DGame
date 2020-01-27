8a9d5bbaa3dc17ca20bab245923bdd563b59e7e3
Pedro Cisneros Santana <pedro.cisneros.santana@gmail.com>
2020-01-27 01:05:19 +0100

    Update Game.java
    game loop

--- src/dev/wannadie/tilegame/Game.java
+++ src/dev/wannadie/tilegame/Game.java
@@ -4,27 +4,64 @@ public class Game implements Runnable {
 
   private Display display;
 
+  private boolean running = false;
+
+  public String title;
   public int width;
   public int height;
 
   private Thread thread;
 
   public Game(String title, int width, int height) {
+	this.title = title;
 	this.width = width;
 	this.height = height;
+  }
+
+  private void init() {
 	display = new Display(title, width, height);
   }
 
-  public void run() {
+  private void update() {
 
+  }
+
+  private void render() {
+
+  }
+
+  public void run() {
+	init();
+
+	while(running) {
+	  update();
+	  render();
+	}
+	
+	stop();
   }
 
   public synchronized void start() {
-	thread = new Thread();
-	thread.start();
+	if (running) {
+	  return;
+	} else {
+	  running = true;
+	  thread = new Thread();
+	  thread.start(); 
+	}
   }
 
   public synchronized void stop() {
+	if (!running) {
+	  return;
+	} else {
+	  running = false;
+	  try {
+		thread.join();
+	  } catch (InterruptedException e) {
+		e.printStackTrace();
+	  }
+	}
 
   }
 
